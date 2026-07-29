24bdf6f66b95847ae7392a8ed3f990824a31eff2
Keerthana P S <keerthanaps0106@gmail.com>
2026-07-29 09:53:45 +0530

    Task5

--- Task5.java
+++ Task5.java
@@ -1,21 +1,31 @@
 import java.util.Arrays;
-
+import java.util.Scanner;
 public class Task5 {
-    public static int findKthSmallest(int[] arr, int k) {
-        
-        if (arr == null || k > arr.length || k <= 0) {
-            throw new IllegalArgumentException("Invalid value of k or empty array");
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr);
+        System.out.print("Enter the value of k: ");
+        int k = sc.nextInt();
 
-        return arr[k - 1];
-    }
+        if (k <= 0 || k > n) {
+            System.out.println("Invalid value of k");
+        } else {
+            Arrays.sort(arr);
+            System.out.println("The " + k + "th smallest element is: " + arr[k - 1]);
+        }
 
-    public static void main(String[] args) {
-        int[] arr = {12, 3, 5, 7, 19, 1};
-        int k = 3;
-        
-        System.out.println("The " + k + "rd smallest element is: " + findKthSmallest(arr, k));
+        sc.close();
     }
 }
