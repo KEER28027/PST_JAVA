01a9db6bc7b509fc3c44183011482d6c5a387e70
Keerthana P S <keerthanaps0106@gmail.com>
2026-07-29 09:43:42 +0530

    Task7

--- Task7.java
+++ Task7.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
 public class Task7 {
     public static void main(String[] args) {
-        int number = 123456;
 
-        int evenSum = String.valueOf(number).chars()
-                .map(Character::getNumericValue)
-                .filter(digit -> digit % 2 == 0)
-                .sum();
+        Scanner sc = new Scanner(System.in);
 
-        int oddSum = String.valueOf(number).chars()
-                .map(Character::getNumericValue)
-                .filter(digit -> digit % 2 != 0)
-                .sum();
+        int number = sc.nextInt();
+        int evenSum = 0;
+        int oddSum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+
+            if (digit % 2 == 0) {
+                evenSum = evenSum + digit;
+            } else {
+                oddSum = oddSum + digit;
+            }
+
+            number = number / 10;
+        }
 
         System.out.println("Even digits sum: " + evenSum);
         System.out.println("Odd digits sum: " + oddSum);
