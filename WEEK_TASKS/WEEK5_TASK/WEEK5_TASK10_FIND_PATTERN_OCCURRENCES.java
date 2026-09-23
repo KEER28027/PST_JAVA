8416e2f515c0cceaa51a552c0ac6fb68dbd77349
Keerthana P S <keerthanaps0106@gmail.com>
2026-09-23 10:50:13 +0530

    WEEK 5

---
+++ WEEK_TASKS/WEEK5_TASK/WEEK5_TASK10_FIND_PATTERN_OCCURRENCES.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class WEEK5_TASK10_FIND_PATTERN_OCCURRENCES {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.nextLine();
+        String pattern = sc.nextLine();
+
+        for (int i = 0; i <= text.length() - pattern.length(); i++) {
+
+            int j;
+
+            for (j = 0; j < pattern.length(); j++) {
+
+                if (text.charAt(i + j) != pattern.charAt(j)) {
+                    break;
+                }
+            }
+
+            if (j == pattern.length()) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
---
+++ WEEK_TASKS/WEEK5_TASK/WEEK5_TASK9_STRING_MATCHING_IN_ARRAY.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class WEEK5_TASK9_STRING_MATCHING_IN_ARRAY {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String[] words = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            words[i] = sc.next();
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (i != j && words[j].contains(words[i])) {
+                    System.out.print(words[i] + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
