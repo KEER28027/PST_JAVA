4ab4ebadb0f8c90b5248bacec19bc0b53699b9ce
Keerthana P S <keerthanaps0106@gmail.com>
2026-09-23 08:48:54 +0530

    OUTPUT

---
+++ WEEK_TASKS/WEEK4_TASK/WEEK4_TASK4_CONTAINS_DUPLICATE.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class WEEK4_TASK4_CONTAINS_DUPLICATE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        HashSet<Integer> set = new HashSet<>();
+
+        boolean duplicate = false;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+
+            if (set.contains(x))
+                duplicate = true;
+
+            set.add(x);
+        }
+
+        System.out.println(duplicate);
+    }
+}
